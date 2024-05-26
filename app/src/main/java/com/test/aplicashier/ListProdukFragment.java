60dfc482e3103bbe57a91d83cce56a0f65d46e89
LinusArdell <linus2016chaesa@mhs.mdp.ac.id>
2024-05-26 16:23:23 +0700

    Upgrade dataset

--- app/src/main/java/com/test/aplicashier/ListProdukFragment.java
+++ app/src/main/java/com/test/aplicashier/ListProdukFragment.java
@@ -34,12 +34,25 @@ public class ListProdukFragment extends Fragment {
         produkList.add(new Produk("8992753282401","123 BENDERA COKLAT 300G", 19600));
         produkList.add(new Produk("711844110069","ABC KECAP MANIS SEDANG 620ML", 17400));
         produkList.add(new Produk("711844120105","ABC SAMBAL MANIS PEDAS 135ML", 4200));
+
+
+//        produkList.add(new Produk("","", ));
+        produkList.add(new Produk("8999909192034","2.3.4 FILTER",86000));
+        produkList.add(new Produk("711844150003","ABC SYRUP APOLO 580ML ORANGE", 11200));
+        produkList.add(new Produk("8992772122245","ADAM SARI 1PAPAN", 24800));
+        produkList.add(new Produk("8999918183085","AIM BISC 120G BUTTER COCONUT", 2900));
+        produkList.add(new Produk("8993560066116","AIR WICK CLICK SPR 15ML LAVENDER", 15400));
+        produkList.add(new Produk("9555540000979","ALFREDO 100G SGR FREE ALMOND", 18500));
+        produkList.add(new Produk("8993417374234","ESKULIN KIDS F.BODY WASH 175ML SPRI", 11600));
+        produkList.add(new Produk("9556852990323","EVER D.COOKIES 454G DANISH(BR MUDA)", 27000));
+
         // Tambahkan produk lainnya sesuai kebutuhan
 
         produkAdapter = new ProdukAdapter(produkList);
         recyclerViewProduk.setAdapter(produkAdapter);
 
         // Setup SearchView
+        searchViewProduk.setIconifiedByDefault(false);
         searchViewProduk.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
